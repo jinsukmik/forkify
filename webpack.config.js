790290953cc5ac7c2b93864298599ccb69ca191c
jinsukmik <jinsukmik@gmail.com>
2019-02-03 23:59:21 -0600

    finished the webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
+// DEPENDENCIES
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// exporting this configuration
 module.exports = {
+    // entry point is where webpack starts the bundling
     entry: './src/js/index.js',
+    // where to save the bundled file with path to folder and the file name
     output: {
-        path: path.resolve(_dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(_dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
-    mode: 'development'
+    // specify the folder where webpack should serve our files
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                // look for all files and test if they end in .js
+                test: /\.js$/,
+                // exclude node_modules folder
+                exclude: /node_modules/,
+                // and use babel-loader on it
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 }
