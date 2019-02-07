9506da0ef48643fe96cd3bca4cc504461d141918
jinsukmik <jinsukmik@gmail.com>
2019-02-06 22:01:31 -0600

    added webpack & babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,47 @@
-// DEPENDENCIES
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// exporting this configuration
+// exporting this object
 module.exports = {
-    // entry point is where webpack starts the bundling
-    entry: ['babel-polyfill', './src/js/index.js'],
-    // where to save the bundled file with path to folder and the file name
+    // where webpack starts looking for all the dependencies which it should then bundle together
+    entry: ['babel-polyfill','./src/js/index.js'],
+    // tell where webpack should put the bundle file
     output: {
-        path: path.resolve(_dirname, 'dist'),
+        // needs absolute path so we use path 
+        path: path.resolve(__dirname, 'dist'),
+        // name of the file webpack is creating
         filename: 'js/bundle.js'
     },
-    // specify the folder where webpack should serve our files
     devServer: {
+        // the folder webpack should serve our files in
         contentBase: './dist'
     },
+    // allows us to do complex processing of our input files
     plugins: [
+        // loading & complex processing of our input index.html
         new HtmlWebpackPlugin({
+            //name of the file
             filename: 'index.html',
+            // template, starting, html file
             template: './src/index.html'
         })
     ],
+    //standard babel-loader configuration https://github.com/babel/babel-loader
     module: {
+        // receives an array of all the loaders we want to use
         rules: [
             {
-                // look for all files and test if they end in .js
-                test: /\.js$/,
-                // exclude node_modules folder
-                exclude: /node_modules/,
-                // and use babel-loader on it
+                // look for all of the files and test if they end with .js(regular expression below)
+                test: /\.m?.js$/,
+                // avoid babel from going through all the node_modules files
+                exclude: /(node_modules|bower_components)/,
+                //use this loader when you find .js files
                 use: {
-                    loader: 'babel-loader'
+                    loader: 'babel-loader',
+                    options: {
+                        preset: ['@babel/preset-env']
+                    }
                 }
             }
         ]
     }
-}
+};
